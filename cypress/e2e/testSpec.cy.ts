5f16f8d12df38c7e8d1dcd4dea13f6d62f72535d
Matěj Štolfa <matej@inuits.eu>
2022-06-22 15:11:32 +0200

    cypress tests for buttons

--- cypress/e2e/testSpec.cy.ts
+++ cypress/e2e/testSpec.cy.ts
@@ -1,5 +1,37 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+import cypressConfig from '../../cypress.config';
+
+describe('Buttons working', () => {
+  it('Testing number buttons', () => {
+    cy.visit('http://localhost:19006/');
+    for (let i = 0; i < 10; i++) {
+      cy.get(`[data-testid=${i}]`).click();
+    }
+    cy.get('[data-testid="currNum"]').should('have.text', '0123456789');
+  });
+
+  it('Testing special number functions', () => {
+    cy.visit('http://localhost:19006/');
+
+    for (let i = 0; i < 5; i++) {
+      cy.get(`[data-testid=${i}]`).click();
+    }
+
+    cy.get('[data-testid=DEL]').click();
+
+    cy.get('[data-testid="currNum"]').should('have.text', '0123');
+
+    cy.get('[data-testid=CE]').click();
+
+    cy.get('[data-testid="currNum"]').should('have.text', '');
+  });
+
+  it('Testing the operation buttons', () => {
+    cy.visit('http://localhost:19006/');
+
+    const input = ['1', '+', '4', '-', '2', '/', '4', 'x', '8'];
+
+    for (let i = 0; i < input.length; i++) {
+      cy.get(`[data-testid=${input[i]}]`).click();
+    }
+  });
+});
